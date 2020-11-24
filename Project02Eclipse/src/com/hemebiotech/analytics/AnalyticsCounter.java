26faf10b7b2b4fafb94806f8d0326113ac51d72b
chyuga9 <cedric.campagna@gmail.com>
2020-11-24 18:21:10 +0100

    Chemin texte fichier exact

--- Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
+++ Project02Eclipse/src/com/hemebiotech/analytics/AnalyticsCounter.java
@@ -8,15 +8,10 @@ public class AnalyticsCounter {
 	private static int headacheCount = 0; // initialize to 0
 	private static int rashCount = 0; // initialize to 0
 	private static int pupilCount = 0; // initialize to 0
-	// Commentaire
-	// un nouveau commentaire
-	// encore un autre commentaire
-	// Un 4e commentaire
-	// 5e commentaire
 
 	public static void main(String args[]) throws Exception {
 		// first get input
-		BufferedReader reader = new BufferedReader(new FileReader("symptoms.txt"));
+		BufferedReader reader = new BufferedReader(new FileReader("Project02Eclipse/symptoms.txt"));
 		String line = reader.readLine();
 
 		int i = 0; // set i to 0
